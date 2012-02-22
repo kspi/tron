16ed755f7e5841340aa4ddc449c26ef613e58bcd
Kiprianas Spiridonovas <k.spiridonovas@gmail.com>
2012-02-22 16:53:04 +0200

    Improve simutron module.

--- simutron.py
+++ simutron.py
@@ -5,6 +5,12 @@ import sys
 import game
 import ai
 
+def run_game(rows, columns, ais):
+    players = [ai.AIPlayer(a) for a in ais]
+    tron = game.Tron((rows, columns), players)
+    winner = tron.run()
+    return (winner, tron.turn)
+
 if __name__ == '__main__':
     if len(sys.argv) < 2 or '--help' in sys.argv or '-h' in sys.argv:
         print 'Usage: %s ROWS COLUMNS AI [AI2 AI3 ...]' % sys.argv[0]
@@ -12,7 +18,5 @@ if __name__ == '__main__':
     rows = int(sys.argv[1])
     columns = int(sys.argv[2])
     ais = sys.argv[3:]
-    players = [ai.AIPlayer(a) for a in ais]
-    tron = game.Tron((rows, columns), players)
-    winner = tron.run()
-    print '%s won after %d turns.' % (tron.players[winner].name, tron.turn)
+    winner, turn = run_game(rows, columns, ais) 
+    print '%d (%s) won after %d turns.' % (winner, ais[winner], turn)
